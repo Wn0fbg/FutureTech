ef4af7ee906edfad6d4876dddc2ec67d2e966f70
Wn0fbg <temakozlovofficiall@gmail.com>
2025-12-28 19:48:10 +0300

    Project finished

--- scripts/BaseComponent.js
+++ scripts/BaseComponent.js
@@ -1,37 +1,35 @@
 class BaseComponent {
   constructor() {
     if (this.constructor === BaseComponent) {
-      throw new Error(
-        "Невозможно создать экземпляр абстрактного класса BaseComponent!"
-      );
+      throw new Error('Невозможно создать экземпляр абстрактного класса BaseComponent!')
     }
   }
 
   getProxyState(initialState) {
     return new Proxy(initialState, {
       get: (target, prop) => {
-        return target[prop];
+        return target[prop]
       },
       set: (target, prop, newValue) => {
-        const oldValue = target[prop];
+        const oldValue = target[prop]
 
-        target[prop] = newValue;
+        target[prop] = newValue
 
         if (newValue !== oldValue) {
-          this.updateUI();
+          this.updateUI()
         }
 
-        return true;
+        return true
       },
-    });
+    })
   }
 
   /**
    * Перерисовка UI в ответ на обновление состояния
    */
   updateUI() {
-    throw new Error("Необходимо реализовать метод updateUI!");
+    throw new Error('Необходимо реализовать метод updateUI!')
   }
 }
 
-export default BaseComponent;
+export default BaseComponent
--- scripts/VideoPlayer.js
+++ scripts/VideoPlayer.js
@@ -1,54 +1,52 @@
-const rootSelector = "[data-js-video-player]";
+const rootSelector = '[data-js-video-player]'
 
 class VideoPlayer {
   selectors = {
     root: rootSelector,
-    video: "[data-js-video-player-video]",
-    panel: "[data-js-video-player-panel]",
-    playButton: "[data-js-video-player-play-button]",
-  };
+    video: '[data-js-video-player-video]',
+    panel: '[data-js-video-player-panel]',
+    playButton: '[data-js-video-player-play-button]',
+  }
 
   stateClasses = {
-    isActive: "is-active",
-  };
+    isActive: 'is-active',
+  }
 
   constructor(rootElement) {
-    this.rootElement = rootElement;
-    this.videoElement = this.rootElement.querySelector(this.selectors.video);
-    this.panelElement = this.rootElement.querySelector(this.selectors.panel);
-    this.playButtonElement = this.rootElement.querySelector(
-      this.selectors.playButton
-    );
-    this.bindEvents();
+    this.rootElement = rootElement
+    this.videoElement = this.rootElement.querySelector(this.selectors.video)
+    this.panelElement = this.rootElement.querySelector(this.selectors.panel)
+    this.playButtonElement = this.rootElement.querySelector(this.selectors.playButton)
+    this.bindEvents()
   }
 
   onPlayButtonClick = () => {
-    this.videoElement.play();
-    this.videoElement.controls = true;
-    this.panelElement.classList.remove(this.stateClasses.isActive);
-  };
+    this.videoElement.play()
+    this.videoElement.controls = true
+    this.panelElement.classList.remove(this.stateClasses.isActive)
+  }
 
   onVideoPause = () => {
-    this.videoElement.controls = false;
-    this.panelElement.classList.add(this.stateClasses.isActive);
-  };
+    this.videoElement.controls = false
+    this.panelElement.classList.add(this.stateClasses.isActive)
+  }
 
   bindEvents() {
-    this.playButtonElement.addEventListener("click", this.onPlayButtonClick);
-    this.videoElement.addEventListener("pause", this.onVideoPause);
+    this.playButtonElement.addEventListener('click', this.onPlayButtonClick)
+    this.videoElement.addEventListener('pause', this.onVideoPause)
   }
 }
 
 class VideoPlayerCollection {
   constructor() {
-    this.init();
+    this.init()
   }
 
   init() {
     document.querySelectorAll(rootSelector).forEach((element) => {
-      new VideoPlayer(element);
-    });
+      new VideoPlayer(element)
+    })
   }
 }
 
-export default VideoPlayerCollection;
+export default VideoPlayerCollection
